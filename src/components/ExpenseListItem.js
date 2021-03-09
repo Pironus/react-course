093bc358a836b88d4f0289b8bcc3ae2a9542f678
Stanislas Chaillou <sfjchaillou@gmail.com>
2021-03-09 18:21:03 +0100

    new stuff

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,19 @@ import {Link} from 'react-router-dom'
 import { connect } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({description,amount,createdAt,id}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
 
